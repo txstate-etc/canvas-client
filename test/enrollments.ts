cf282b1433593458f18d9cc5d74394df29af34aa
Nick Wing <nwing@txstate.edu>
2020-01-08 10:54:19 -0600

    improve the enrollments test to make sure course id matches

--- test/enrollments.ts
+++ test/enrollments.ts
@@ -7,11 +7,12 @@ import { canvasAPI } from '../src'
 describe('enrollments', function () {
   it('should retrieve enrollments from a course', async () => {
     const courses = await canvasAPI.getUserCourses()
-    const enrollments = await canvasAPI.getCourseEnrollments(courses[0].id)
+    const courseId = courses[0].id
+    const enrollments = await canvasAPI.getCourseEnrollments(courseId)
     expect(enrollments).to.have.length.greaterThan(0)
     for (const enrollment of enrollments) {
       expect(enrollment.id).to.be.greaterThan(0)
-      expect(enrollment.course_id).to.be.greaterThan(0)
+      expect(enrollment.course_id).to.equal(courseId)
     }
   })
 })
