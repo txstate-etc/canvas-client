3b3f017b74369662ed60eca9cf42a902c8bede8a
Nick Wing <nwing@txstate.edu>
2021-06-15 10:12:37 -0500

    version upgrade

--- test/enrollments.ts
+++ test/enrollments.ts
@@ -22,7 +22,7 @@ describe('enrollments', function () {
       expect(enrollment.id).to.be.greaterThan(0)
       expect(enrollment.course_id).to.be.greaterThan(0)
     }
-  }).timeout(10000)
+  }).timeout(15000)
   it('should retrieve enrollments from a user', async () => {
     const enrollments = await canvasAPI.getUserEnrollments('self')
     expect(enrollments).to.have.length.greaterThan(0)
